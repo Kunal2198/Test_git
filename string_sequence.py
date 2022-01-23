a48c7fdf73db3fe77b2a31e55fd0dae5b397d391
kunal suryawanshi <kunal.suryawanshi21@gmail.com>
2022-01-23 16:27:15 +0530

    String Manulplation

---
+++ string_sequence.py
@@ -0,0 +1,15 @@
+low=1000
+high=11000
+digits='123456789'
+seq = []
+nl = len(str(low)) #4
+hl = len(str(high)) #5
+
+for i in range(nl,hl+1): #4
+    for j in range(0,10-i): #(0,6)->0to5 j=0
+        num = int(digits[j:j+i])  #digits[0:0+4]
+        if num>low & num<high:
+            seq.append(num)
+
+
+print(seq)
